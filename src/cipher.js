40e20a49ed8c2eeaf9dd5d4f82c23dde9bafd6f6
erickalemos <ericka.florentino@gmail.com>
2019-08-26 08:42:19 -0300

    alterações de padronização

--- src/cipher.js
+++ src/cipher.js
@@ -1,30 +1,24 @@
 window.cipher = {
   // ... 
-  encode:encode,
-  decode:decode
- }
-  	
-	function encode(codificar,offset){
-			let exibir = " ";
-			for (let i= 0;i < codificar.length;i++) {
-			let alterar = codificar[i].charCodeAt();
-			let teste =((alterar-65+offset )% 26)+65;
-			exibir += String.fromCharCode(teste);
-					
-		}
-		return exibir;
-  	}
-  	
-  	function decode(decodificar,offset){
-  		let exibir2 = " ";
-  		for (let i= 0;i < decodificar.length;i++) {
-  		let alterar2 = decodificar[i].charCodeAt();
-  		let teste2 = ((alterar2-90-offset)%26)+90;
-  		exibir2 += String.fromCharCode(teste2);
-  		
-  		}
-  		return exibir2;
-  	}
-
-
-  
+  encode: encode,
+  decode: decode
+};
+//if (codificar = )
+function encode( codificar, offset) {
+  let exibir = " ";
+  for (let i= 0;i < codificar.length;i++) {
+    let alterar = codificar[i].charCodeAt();
+    let teste =((alterar-65+offset )% 26)+65;
+    exibir += String.fromCharCode(teste);
+  }
+  return exibir;//.replace(":"," ")
+}
+function decode( decodificar, offset) {
+  let exibir2 = " ";
+  for (let i= 0;i < decodificar.length;i++) {
+    let alterar2 = decodificar[i].charCodeAt();
+    let teste2 = ((alterar2-90-offset)%26)+90;
+    exibir2 += String.fromCharCode(teste2);
+  }
+  return exibir2;
+}  
--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 
-
-
-function clique(){
-	let codificar=document.getElementById("codificar").value;
-	let offset=Number(document.getElementById("offset").value);
-	let exibir = cipher.encode(codificar,offset);
-	document.getElementById("mensagem").innerHTML =`teste ${exibir}`;
-
-
+document.addEventListener(clique);
+function clique() {
+  let codificar = document.getElementById( "codificar").value;
+  let offset = Number(document.getElementById( "offset").value);
+  let exibir = cipher.encode( codificar, offset);
+  //let space = codificar.replace(" ", " "); 
+  document.getElementById( "mensagem").innerHTML = `${exibir}`;
 }
-
-function clique2(){
-	let decodificar=document.getElementById("decodificar").value;
-	let offset=Number(document.getElementById("offset").value);
-	let exibir2 = cipher.decode(decodificar,offset);
-	document.getElementById("mensagem2").innerHTML =`teste ${exibir2}`;	
+document.addEventListener(clique2);
+function clique2() {
+  let decodificar = document.getElementById( "decodificar").value;
+  let offset = Number(document.getElementById( "offset").value);
+  let exibir2 = cipher.decode( decodificar, offset);
+  document.getElementById( "mensagem2").innerHTML = `${exibir2}`;	
 }
