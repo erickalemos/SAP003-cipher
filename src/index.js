2bba254e55a55b317e8278b75c3a1af553c5e6a2
erickalemos <ericka.florentino@gmail.com>
2019-08-26 21:40:29 -0300

    alterações do CSS

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 
 document.addEventListener(clique);
 function clique() {
-  let codificar = document.getElementById( "codificar").value;
   let offset = Number(document.getElementById( "offset").value);
+  let codificar = document.getElementById( "codificar").value;
   let exibir = cipher.encode( codificar, offset);
   //let space = codificar.replace(" ", " "); 
   document.getElementById( "mensagem").innerHTML = `${exibir}`;
 }
 document.addEventListener(clique2);
 function clique2() {
-  let decodificar = document.getElementById( "decodificar").value;
   let offset = Number(document.getElementById( "offset").value);
+  let decodificar = document.getElementById( "decodificar").value;
   let exibir2 = cipher.decode( decodificar, offset);
   document.getElementById( "mensagem2").innerHTML = `${exibir2}`;	
 }
