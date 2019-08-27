7c413dd2631b2a1194179a155c3dbfc4e451fdc3
erickalemos <ericka.florentino@gmail.com>
2019-08-27 15:02:20 -0300

    CORREÇÃO DE ESPAÇO

--- src/cipher.js
+++ src/cipher.js
@@ -5,7 +5,7 @@ window.cipher = {
 };
 
 function encode( offset, codificar ) {
-  let exibir = " ";
+  let exibir = "";
   for (let i= 0;i < codificar.length;i++) {
     let alterar = codificar[i].charCodeAt();
     let teste =((alterar-65+offset )% 26)+65;
@@ -14,7 +14,7 @@ function encode( offset, codificar ) {
   return exibir;
 }
 function decode( offset, decodificar) {
-  let exibir2 = " ";
+  let exibir2 = "";
   for (let i= 0;i < decodificar.length;i++) {
     let alterar2 = decodificar[i].charCodeAt();
     let teste2 = ((alterar2-90-offset)%26)+90;
