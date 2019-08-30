fb57e692b198b7808a57036b21c3cfccfcb71949
erickalemos <ericka.florentino@gmail.com>
2019-08-30 15:18:15 -0300

    atualização das minusculas e caracteres especiais

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,42 @@
 window.cipher = {
-  // ... 
   encode: encode,
   decode: decode
 };
 
-function encode( offset, codeMsg ) {
-  let showMsg = ""; 
+function encode(offset, codeMsg) {
+  let resultShowMsg = "";
+  
   for (let i= 0;i < codeMsg.length;i++) {
     let letterNumber = codeMsg[i].charCodeAt();
-    let calc =(( letterNumber-65+offset)%26)+65;
-    showMsg += String.fromCharCode(calc);
-  } 
-  return showMsg;
+    let calc =(( letterNumber-65+offset) %26) +65;
+    if (letterNumber >= 65 && letterNumber <= 90) {
+      resultShowMsg += String.fromCharCode(calc);
+    } else if (letterNumber >= 97 && letterNumber <= 122) {
+      let calc2 =(( letterNumber-97+offset) %26) +97;
+      resultShowMsg += String.fromCharCode(calc2);
+    } else {
+      let calc3 =(( letterNumber-32+offset) %33) +32;
+      resultShowMsg += String.fromCharCode(calc3);
+    }
+  }
+  return resultShowMsg;
 }
 
-function decode( offset, decodeMsg) {
-  let showMsgTwo = "";
+function decode(offset, decodeMsg) {
+  let resultShowMsgTwo = "";
   for (let i= 0;i < decodeMsg.length;i++) {
     let letterNumberTwo = decodeMsg[i].charCodeAt();
     let calcTwo = ((letterNumberTwo-90-offset)%26)+90;
-    showMsgTwo += String.fromCharCode(calcTwo);
+    if (letterNumberTwo >= 65 && letterNumberTwo <= 90) {
+      resultShowMsgTwo += String.fromCharCode(calcTwo);
+    } else if (letterNumberTwo >= 97 && letterNumberTwo <= 122) {
+      let calcThree =(( letterNumberTwo-122-offset) %26) +122;
+      resultShowMsgTwo += String.fromCharCode(calcThree);
+    } else {
+      let calcFour =(( letterNumberTwo-64-offset) %33) +64;
+      resultShowMsgTwo += String.fromCharCode(calcFour);
+    }
   }
-  return showMsgTwo;
+  return resultShowMsgTwo;
 }  
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 
-document.getElementById("btn").addEventListener( "click", press);
-function press() {
-  let offset = Number(document.getElementById( "offset").value);
-  let codeMsg = document.getElementById( "codeMsg").value;
-  let showMsg = cipher.encode( offset, codeMsg);
-  document.getElementById( "msgOne").innerHTML = `${showMsg}`;
+document.getElementById("btn").addEventListener("click", pressEncode);
+document.getElementById("btn2").addEventListener("click", pressDecode);
+
+function pressEncode() {
+  let offset = Number(document.getElementById("offset").value);
+  let codeMsg = document.getElementById("codeMsg").value;
+  let resultShowMsg = window.cipher.encode(offset, codeMsg);
+  document.getElementById("msgOne").innerHTML = `<p> ${resultShowMsg} </p>`;
 }
-document.getElementById("btn").addEventListener( "click", pressTwo);
-function pressTwo() {
-  let offset = Number(document.getElementById( "offset").value);
+
+function pressDecode() {
+  let offset = Number(document.getElementById("offset").value);
   let decodeMsg = document.getElementById( "decodeMsg").value;
-  let showMsgTwo = cipher.decode( offset, decodeMsg);
-  document.getElementById( "msgTwo").innerHTML = `${showMsgTwo}`;	
+  let resultShowMsgTwo = window.cipher.decode(offset, decodeMsg);
+  document.getElementById("msgTwo").innerHTML = `${resultShowMsgTwo}`;	
 }
