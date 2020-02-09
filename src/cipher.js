69ae7cd9aa77d5b8a1bc419630f40c102fe883b6
erickalemos <ericka.florentino@gmail.com>
2020-02-08 22:29:46 -0300

    refatoração de nomes

--- src/cipher.js
+++ src/cipher.js
@@ -2,46 +2,42 @@ window.cipher = {
   encode: encode,
   decode: decode
 };
-//Função de codificação
+
 function encode(offset, codeMsg) {
-  let showMsg = "";
-  //loop for que prossegue codificando até a ultima letra digitada
-  //let showMsg é a variavél que vai printar a msg na tela
+  let showMsgCode = "";
   //let letterNumber transforma a variável em um número
-  //let calc aplica o cálculo da cifra de César 
   for (let i= 0;i < codeMsg.length;i++) {
     let letterNumber = codeMsg[i].charCodeAt();
     let calc =(( letterNumber-65+offset) %26) +65;
     // condicional if para pegar as letras maiúsculas, else if minúsculas e else para o espaço
     if (letterNumber >= 65 && letterNumber <= 90) {
-      showMsg += String.fromCharCode(calc);
+      showMsgCode += String.fromCharCode(calc);
     } else if (letterNumber >= 97 && letterNumber <= 122) {
       calc =(( letterNumber-97+offset) %26) +97;
-      showMsg += String.fromCharCode(calc);
+      showMsgCode += String.fromCharCode(calc);
     } else {
       calc =(( letterNumber-32+offset) %33) +32;
-      showMsg += String.fromCharCode(calc);
+      showMsgCode += String.fromCharCode(calc);
     }
   }
-  return showMsg;
+  return showMsgCode;
 }
-//Função de decodificação
+
 function decode(offset, decodeMsg) {
-  let showMsgTwo = "";
-  //loop for que prossegue decodificando até a ultima letra digitada
+  let showMsgDecode = "";
   for (let i= 0;i < decodeMsg.length;i++) {
-    let letterNumberTwo = decodeMsg[i].charCodeAt();
-    let calcTwo = ((letterNumberTwo-90-offset)%26)+90;
-    if (letterNumberTwo >= 65 && letterNumberTwo <= 90) {
-      showMsgTwo += String.fromCharCode(calcTwo);
-    } else if (letterNumberTwo >= 97 && letterNumberTwo <= 122) {
-      let calcTwo =(( letterNumberTwo-122-offset) %26) +122;
-      showMsgTwo += String.fromCharCode(calcTwo);
+    let letterNumberDecode = decodeMsg[i].charCodeAt();
+    let calcDecode = ((letterNumberDecode-90-offset)%26)+90;
+    if (letterNumberDecode >= 65 && letterNumberDecode <= 90) {
+      showMsgDecode += String.fromCharCode(calcDecode);
+    } else if (letterNumberDecode >= 97 && letterNumberDecode <= 122) {
+      let calcDecode =(( letterNumberDecode-122-offset) %26) +122;
+      showMsgDecode += String.fromCharCode(calcDecode);
     } else {
-      let calcTwo =(( letterNumberTwo-64-offset) %33) +64;
-      showMsgTwo += String.fromCharCode(calcTwo);
+      let calcDecode =(( letterNumberDecode-64-offset) %33) +64;
+      showMsgDecode += String.fromCharCode(calcDecode);
     }
   }
-  return showMsgTwo;
+  return showMsgDecode;
 }  
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 
 document.getElementById("btn").addEventListener("click", pressEncode);
 document.getElementById("btn").addEventListener("click", pressDecode);
-//Função que recebe as informações do usuário para codificar, articula com a função encode no cipher.js e retorna o resultado para o usuário.
+
 function pressEncode() {
   let offset = Number(document.getElementById("offset").value);
   let codeMsg = document.getElementById("codeMsg").value;
-  let showMsg = window.cipher.encode(offset, codeMsg);
-  document.getElementById("msgOne").innerHTML = `<p> ${showMsg} </p>`;
+  let showMsgCode = window.cipher.encode(offset, codeMsg);
+  document.getElementById("msgOne").innerHTML = `<p> ${showMsgCode} </p>`;
 }
-//Função que recebe as informações do usuário para decodificar, articula com a função decode no cipher.js e retorna o resultado para o usuário.
+
 function pressDecode() {
   let offset = Number(document.getElementById("offset").value);
   let decodeMsg = document.getElementById( "decodeMsg").value;
-  let showMsgTwo = window.cipher.decode(offset, decodeMsg);
-  document.getElementById("msgTwo").innerHTML = `${showMsgTwo}`;	
+  let showMsgDecode = window.cipher.decode(offset, decodeMsg);
+  document.getElementById("msgTwo").innerHTML = `${showMsgDecode}`;	
 }
